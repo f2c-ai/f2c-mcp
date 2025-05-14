0b51847400ad1c3fa9fdda4346df44ac8f83329b
xuhongbin <xuhongbin@yy.com>
2025-05-14 20:41:29 +0800

    feat: 增加 单元测试 与 e2e 测试

--- src/server/figma/api.ts
+++ src/server/figma/api.ts
@@ -1,43 +1,62 @@
-import type {GetFileNodesParams, GetFileParams, GetImagesParams, NodeToCodeWithF2C} from './apiTypes'
+import type {
+  GetFileParams,
+  GetImagesParams,
+  GetKeyParams,
+  NodeToCodeWithF2C,
+  NodeToCodeWithF2COptions,
+} from './apiTypes'
+import {DEFAULT_PERSONAL_TOKEN} from './config'
 
 class Api {
   protected figmaHost = `https://api.figma.com/v1`
   protected f2cHost = `https://f2c-figma-api.yy.com/api`
+  private personalToken = DEFAULT_PERSONAL_TOKEN
   //
-  async nodes2Code(o: NodeToCodeWithF2C) {
-    const url = this.opToUrl(`${this.f2cHost}/nodes`, o)
-    return this.fetch(url, o.personal_token, 'text')
+  async f2cNodeToCode(o: NodeToCodeWithF2COptions) {
+    const op: NodeToCodeWithF2C = {
+      fileKey: o.fileKey,
+      nodeIds: o.ids,
+      personal_token: o.personalToken || this.personalToken,
+      format: o.format,
+    }
+
+    const url = this.opToUrl(`${this.f2cHost}/nodes`, op, [])
+    return this.fetch(url, 'text')
   }
-  async files(fileKey: string, personalToken: string, op: GetFileParams) {
-    const url = this.opToUrl(`${this.figmaHost}/files/${fileKey}`, op)
-    return this.fetch(url, personalToken)
+  async files(o: GetFileParams) {
+    let url: string
+    if (o.ids) {
+      url = this.opToUrl(`${this.figmaHost}/files/${o.fileKey}/nodes`, o)
+    } else {
+      url = this.opToUrl(`${this.figmaHost}/files/${o.fileKey}`, o)
+    }
+
+    return this.fetch(url)
   }
-  async nodes(fileKey: string, personalToken: string, op: GetFileNodesParams) {
-    const url = this.opToUrl(`${this.figmaHost}/files/${fileKey}/nodes`, op)
-    return this.fetch(url, personalToken)
-  }
-  async images(fileKey: string, personalToken: string, op: GetImagesParams) {
-    const url = this.opToUrl(`${this.figmaHost}/images/${fileKey}`, op)
-    return this.fetch(url, personalToken)
+  async images(o: GetImagesParams) {
+    const url = this.opToUrl(`${this.figmaHost}/images/${o.fileKey}`, o)
+    return this.fetch(url)
   }
   // Returns download links for all images present in image fills
-  async imageFills(fileKey: string, personalToken: string) {
-    const url = this.opToUrl(`${this.figmaHost}/files/${fileKey}/images`, {})
-    return this.fetch(url, personalToken)
+  async imageFills(o: GetKeyParams) {
+    const url = this.opToUrl(`${this.figmaHost}/files/${o.fileKey}/images`, o)
+    return this.fetch(url)
   }
   // Returns the metadata for the file referred to by :key
-  async meta(fileKey: string, personalToken: string) {
-    const url = this.opToUrl(`${this.figmaHost}/files/${fileKey}/meta`, {})
-    return this.fetch(url, personalToken)
+  async meta(o: GetKeyParams) {
+    const url = this.opToUrl(`${this.figmaHost}/files/${o.fileKey}/meta`, o)
+    return this.fetch(url)
   }
-  async fetch(url: string, personalToken: string, resType: 'json' | 'text' = 'json') {
+  async fetch(url: string, resType: 'json' | 'text' = 'json'): Promise<any> {
     try {
-      const response = await fetch(url, {
+      const fetchOptions = {
         method: 'GET',
         headers: {
-          'X-FIGMA-TOKEN': personalToken,
+          'X-FIGMA-TOKEN': this.personalToken,
         },
-      })
+      }
+      const response = await fetch(url, fetchOptions)
+      // console.log('response', url, JSON.stringify(fetchOptions))
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`)
       }
@@ -48,13 +67,16 @@ class Api {
       throw error
     }
   }
-  private opToUrl(api: string, o: object = {}) {
+  private opToUrl(api: string, o: any = {}, filters = ['fileKey', 'personalToken']) {
     if (Object.keys(o).length === 0) {
       return api
     }
+    if (o.personalToken) {
+      this.personalToken = o.personalToken
+    }
     const url: any = new URL(api)
     for (const [key, value] of Object.entries(o)) {
-      url.searchParams.append(key, value)
+      if (!filters.includes(key)) url.searchParams.append(key, value)
     }
     return url.toString()
   }
