cf3dab8f3f68bfa878837ce558d4f40c35a5775b
xuhongbin <xuhongbin@yy.com>
2025-11-12 17:01:07 +0800

    refactor: 移除stdio和express相关代码及配置
    清理不再使用的stdio和express服务器实现，移除相关代码和package.json中的配置项

--- src/stdio.ts
+++
@@ -1,5 +0,0 @@
-import {StdioServerTransport} from '@modelcontextprotocol/sdk/server/stdio.js'
-import {server} from 'src/tool'
-
-const transport = new StdioServerTransport()
-server.connect(transport)
